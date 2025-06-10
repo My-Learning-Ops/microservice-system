9cec888834d9096de40ea5a9b190c82ca5f31242
Brendan Dileo <dileob23@gmail.com>
2025-06-10 19:11:37 -0400

    Add POST route to create a comic

--- comic-service/server.js
+++ comic-service/server.js
@@ -1,20 +1,47 @@
 
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const Comic = require('./models/comicModel');
 const connectDB = require('./db/connect');
 
+// Setup app and set port
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Basic middleware
 app.use(cors());
 app.use(express.json());
+console.log(process.env.PORT);
+console.log(process.env.MONGO_URI);
 
+// GET all
 app.get('/', (req, res) => {
     res.send('Server up and running.');
 });
 
-console.log(process.env.MONGO_URI);
+// POST a comic
+app.post('/', async (req, res) => {
+    const { title, author, issue, volume, releaseDate } = req.body;
+    if (!title) return res.status(400).json({ message: 'Title is required for this type of request!' });
+
+    try {
+        const comic = await Comic.create({
+            title: title,
+            author: author,
+            issue: issue,
+            volume: volume,
+            releaseDate: releaseDate
+        });
+
+        console.log('Comic Created Successfully!');
+        return res.status(201).json({ message: 'Comic entry created successfully!' });
+    } catch (error) {
+        console.error(`An error occurred: ${error.message}`);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 // Connect to the db
 connectDB().then(() => {
     app.listen(port, () => {
